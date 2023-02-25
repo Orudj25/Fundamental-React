d277e07d0f88e54119a3aafb45065a550b7eb3e9
Orudj25 <orudj_mamedov_199@mail.ru>
2023-02-25 23:40:34 +0400

    fixed bugs with color and style of button

--- src/pages/Posts.jsx
+++ src/pages/Posts.jsx
@@ -10,6 +10,7 @@ import Pagination from "../components/pagination/Pagination";
 import PostService from "../API/PostService";
 import {getPageCount} from "../utils/pages";
 import {useFetching} from "../hooks/useFetching";
+import classes from '../components/UI/button/MyButton.module.css'
 
 
 function Posts() {
@@ -18,31 +19,31 @@ function Posts() {
         {id: 1, title: 'Javascript', body: 'Description'},
         {id: 2, title: 'Javascript 2', body: 'Description'},
     ])
-    const [filter,setFilter] = React.useState({sort:'', query:''})
-    const [modal,setModal] = React.useState('')
-    const sortedAndSearchedPosts = usePosts(posts,filter.sort,filter.query)
-    const [totalPages,setTotalPages] = React.useState(0)
-    const [limit,setLimit] = React.useState(10)
-    const [page,setPage] = React.useState(1)
+    const [filter, setFilter] = React.useState({sort: '', query: ''})
+    const [modal, setModal] = React.useState('')
+    const sortedAndSearchedPosts = usePosts(posts, filter.sort, filter.query)
+    const [totalPages, setTotalPages] = React.useState(0)
+    const [limit, setLimit] = React.useState(10)
+    const [page, setPage] = React.useState(1)
 
-    const [fetchPosts,isPostsLoading,postError] =
-        useFetching(async (limit,page) => {
-            const response = await PostService.getAll(limit,page)
+    const [fetchPosts, isPostsLoading, postError] =
+        useFetching(async (limit, page) => {
+            const response = await PostService.getAll(limit, page)
             setPosts(response.data)
             const totalCount = response.headers['x-total-count']
-            setTotalPages(getPageCount(totalCount,limit))
+            setTotalPages(getPageCount(totalCount, limit))
         })
 
 
     console.log(totalPages)
 
-    React.useEffect(()=>{
-        fetchPosts(limit,page)
-    },[])
+    React.useEffect(() => {
+        fetchPosts(limit, page)
+    }, [])
 
 
     const createPost = (newPost) => {
-        setPosts([...posts,newPost])
+        setPosts([...posts, newPost])
         setModal(false)
     }
 
@@ -53,14 +54,14 @@ function Posts() {
 
     const changePage = (page) => {
         setPage(page)
-        fetchPosts(limit,page)
+        fetchPosts(limit, page)
     }
 
     return (
         <div className="App">
-            <button onClick={fetchPosts}>GET POSTS</button>
+            <MyButton  onClick={fetchPosts}>GET POSTS</MyButton>
             <MyButton
-                style={{marginTop:'30px'}}
+                style={{marginTop: '30px'}}
                 onClick={() => setModal(true)}>
                 Create User
             </MyButton>
@@ -69,7 +70,7 @@ function Posts() {
                 setVisible={setModal}>
                 <PostForm create={createPost}/>
             </MyModal>
-            <hr style={{margin:'15px 0'}}/>
+            <hr style={{margin: '15px 0'}}/>
             <PostFilter
                 filter={filter}
                 setFilter={setFilter}
@@ -78,7 +79,7 @@ function Posts() {
                 <h1>Error ${postError}</h1>
             }
             {isPostsLoading
-                ? <div style={{display:'flex', justifyContent:'center',marginTop:'50px'}}>
+                ? <div style={{display: 'flex', justifyContent: 'center', marginTop: '50px'}}>
                     <Loader/>
                 </div>
                 : <PostList remove={removePost}
